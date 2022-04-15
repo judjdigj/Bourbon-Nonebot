8a3021fc10649ac8307533d2ea3206611f335511
judjdigj <qq1193015946@gmail.com>
2022-04-15 23:23:52 +0800

    Get qqid update

--- bot.py
+++ bot.py
@@ -1,6 +1,12 @@
+from os import path
+
 import nonebot
 
 if __name__ == '__main__':
     nonebot.init()
     nonebot.load_builtin_plugins()
+    nonebot.load_plugins(
+        path.join(path.dirname(__file__), 'bourbon', 'plugins'),
+        'bourbon.plugins'
+    )
     nonebot.run(host='127.0.0.1', port=8080)
